d726f00647035e8a069617414d0c1c1eb95ee14c
chickhld <chickhld@gmail.com>
2024-01-05 20:35:05 +1100

    Update asx.py
    Added year columns

--- asx.py
+++ asx.py
@@ -13,7 +13,7 @@ sns.set()
 start_date_month = '-1-1'
 end_date_month = '-12-30'
 
-start_year = 2022
+start_year = 2020
 end_year = 2023
 range_year = end_year - start_year
 
@@ -24,6 +24,8 @@ datalist = []
 
 plt.figure(figsize=(14,5))
 sns.set_style("ticks")
+
+print('============= Start  ============')
 
 for x in range(range_year):
 
@@ -36,70 +38,70 @@ for x in range(range_year):
     end_range_date.append(end_date)
 
     # extract stock data from yahoo finance
-    datadownland = yf.download(tickers = "AMZN", start = start_date, end = end_date)
+    datadownland = yf.download(tickers = "^AORD", start = start_date, end = end_date)
     datalist.append(datadownland)
 
     # print out downloaded columns
     print('data fields downloaded:', set(datadownland.columns.get_level_values(0)))
 
-    #for col in datadownland.columns:
-    #    print(col)
-
-    #datadownland.reset_index(inplace=True)
-    #print('data fields downloaded:', set(datadownland.columns.get_level_values(0)))
-
-    #print(datadownland.to_string())
-
     # print entries (head takes out the first 5 entries (rows))
+    print('============= Print Entries  ============')
     print(datadownland.head())
     
     # print type
+    print('============= Print Whole Dataframe  ============') 
     print(type(datadownland))
 
-    # print size
+    # print size (250, 6)
+    print('============= Print Dataframe Size ============') 
     print(datadownland.shape)
     
-    # print 
+    # print Index(['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'], dtype='object')
+    print('============= Print Dataframe Columns ============') 
     print(datadownland.columns)
 
     # print types of columns
+    #Open         float64
+    #High         float64
+    #Low          float64
+    #Close        float64
+    #Adj Close    float64
+    #Volume         int64
+    print('============= Print Types of columns ============') 
     print(datadownland.dtypes)
 
     # print 
+    print('============= Print Info ============') 
     print(datadownland.info())
 
     # print index
+    print('============= Print Index ============') 
     print(datadownland.index)
 
-    sns.lineplot(data=datadownland,x="Date",y="High",color='blue',label='AMZ')
+    # add extra row_number column which we can potentially use as index
+    #datadownland = datadownland.assign(row_number=range(len(datadownland)))
+    #print(datadownland.head())
+
+    # add extra row_number column which we can potentially use as index
+    print('============= Print extra day column ============') 
+    datadownland['day'] = datadownland.reset_index().index
+    print(datadownland.head())
+
+    print('============= Print new date column ============') 
+    datadownland = datadownland.assign(date=datadownland.index)
+    print(datadownland.head())
+
+    print('============= Print new year column ============') 
+    datadownland['year'] = pd.DatetimeIndex(datadownland['date']).year
+    print(datadownland.head())
+
+    # Single line plot
+    #sns.lineplot(data=datadownland,x="day",y="High",color='blue')
+
+    #sns.lineplot(data=datadownland,x="day",y="High",hue='year', palette='spring')
+    sns.lineplot(data=datadownland,x="day",y="High",hue='year', palette=['magenta', 'deepskyblue', 'yellowgreen'])
 
 
-plt.title("The Stock Price of Amazon",size='x-large',color='blue')
+plt.title("All Ordinaries Chart",size='x-large',color='blue')
 plt.show()
 
-
-#stock_list = ['0005.HK', '0006.HK', '0066.HK', '0700.HK', '2800.HK']
-#stock_list = ['AMZN']
-#print('stock_list:', stock_list)
-
-#amazon = yf.download(stock_list, start = start_date, end = end_date)
-#amazon = yf.download(tickers = "AMZN", start = start_date, end = end_date)
-
-#print('data fields downloaded:', set(amazon.columns.get_level_values(0)))
-
-# You can see that “Date” is part of our DataFrame index. You need to convert
-# it to a column to plot the data.
-#amazon.reset_index(inplace=True)
-#amazon.head()
-
-# See raw data
-#print(amazon)
-
-#plt.figure(figsize=(14,5))
-
-#sns.set_style("ticks")
-#sns.lineplot(data=amazon,x="Date",y='Close',color='firebrick')
-#sns.despine() # remove top and right lines
-
-#plt.title("The Stock Price of Amazon",size='x-large',color='blue')
-#plt.show()
